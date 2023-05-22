5881269d5b0da18336438d3d639e45273b7148fb
odeyalo <theprogrammerinfuture@gmail.com>
2023-05-22 20:23:38 +0300

    Test to check if spring context bootstraps

---
+++ src/test/java/com/odeyalo/sonata/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
