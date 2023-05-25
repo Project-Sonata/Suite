7f98e2c770b7d8d716e419d3543d36876fbc7d15
odeyalo <theprogrammerinfuture@gmail.com>
2023-05-25 15:52:20 +0300

    Removed Discovery client annotation

--- src/main/java/com/odeyalo/sonata/common/CommonApplication.java
+++ src/main/java/com/odeyalo/sonata/common/CommonApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CommonApplication {
 
     public static void main(String[] args) {
