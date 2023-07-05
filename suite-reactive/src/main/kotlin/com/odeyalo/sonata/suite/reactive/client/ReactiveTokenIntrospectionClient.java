4b426939989bd6bd4399720a71083ce48818c8f8
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-05 16:20:43 +0300

    Added client to introspect client

---
+++ suite-reactive/src/main/kotlin/com/odeyalo/sonata/suite/reactive/client/ReactiveTokenIntrospectionClient.java
@@ -0,0 +1,18 @@
+package com.odeyalo.sonata.suite.reactive.client;
+
+import com.odeyalo.sonata.common.authorization.TokenIntrospectionRequest;
+import com.odeyalo.sonata.common.authorization.TokenIntrospectionResponse;
+import com.odeyalo.sonata.suite.reactive.config.ReactiveFeignConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import reactivefeign.spring.config.ReactiveFeignClient;
+import reactor.core.publisher.Mono;
+
+@ReactiveFeignClient(name = "sonata-authorization", configuration = {ReactiveFeignConfiguration.class})
+public interface ReactiveTokenIntrospectionClient {
+
+    @PostMapping("/token/info")
+    Mono<ResponseEntity<Mono<TokenIntrospectionResponse>>> introspectToken(@RequestBody TokenIntrospectionRequest req);
+
+}
