f74924dfb335779650d2e9cf4c098bd2069aab48
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-09 11:44:01 +0300

    Changed package

--- suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/AbstractEvent.java
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/AbstractEvent.java
@@ -1,10 +1,7 @@
 package com.odeyalo.sonata.suite.brokers.events;
 
-import lombok.AllArgsConstructor;
-
 import java.util.UUID;
 
-@AllArgsConstructor
 public abstract class AbstractEvent<T> implements SonataEvent {
     protected String id;
     protected long creationTime;
@@ -13,6 +10,12 @@ public abstract class AbstractEvent<T> implements SonataEvent {
     public AbstractEvent(T body) {
         this.id = UUID.randomUUID().toString();
         this.creationTime = System.currentTimeMillis();
+        this.body = body;
+    }
+
+    public AbstractEvent(String id, long creationTime, T body) {
+        this.id = id;
+        this.creationTime = creationTime;
         this.body = body;
     }
 
