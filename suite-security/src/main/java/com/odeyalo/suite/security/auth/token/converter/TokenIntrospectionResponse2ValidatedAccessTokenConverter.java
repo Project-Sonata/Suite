d7dd3f91139e2579c96d84f9476257be0da7f936
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-27 21:59:16 +0300

    Added logging

--- suite-security/src/main/java/com/odeyalo/suite/security/auth/token/converter/TokenIntrospectionResponse2ValidatedAccessTokenConverter.java
+++ suite-security/src/main/java/com/odeyalo/suite/security/auth/token/converter/TokenIntrospectionResponse2ValidatedAccessTokenConverter.java
@@ -3,6 +3,8 @@ package com.odeyalo.suite.security.auth.token.converter;
 import com.odeyalo.sonata.common.authorization.TokenIntrospectionResponse;
 import com.odeyalo.suite.security.auth.token.AccessTokenMetadata;
 import com.odeyalo.suite.security.auth.token.ValidatedAccessToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Convert the {@link TokenIntrospectionResponse} to {@link ValidatedAccessToken}
@@ -11,12 +13,17 @@ public class TokenIntrospectionResponse2ValidatedAccessTokenConverter implements
 
     public static final String SCOPE_DELIMITER = " ";
 
+    private final Logger logger = LoggerFactory.getLogger(TokenIntrospectionResponse2ValidatedAccessTokenConverter.class);
+
     @Override
     public ValidatedAccessToken convertTo(TokenIntrospectionResponse body) {
-        System.out.println("CONVERT");
+        this.logger.debug("Convert the body: {} to ValidatedAccessToken token", body);
+
         if (!body.isValid()) {
+            this.logger.debug("The token is invalid. Conversation has been failed and reason is unavailable");
             return ValidatedAccessToken.invalid();
         }
+
         AccessTokenMetadata metadata = AccessTokenMetadata.of(body.getUserId(), body.getScope().split(SCOPE_DELIMITER),
                 body.getIssuedAt(), body.getExpiresIn());
 
