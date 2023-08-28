56c8859d950a1a299ab6882ce9160c2fe8e7c4d4
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2023-08-28 14:45:13 +0300

    Sonata context URI (#13)
    Added context URI for the Sonata Project in common package. Written tests for SonataUri parsers

---
+++ src/main/java/com/odeyalo/sonata/common/context/MalformedContextUriException.java
@@ -0,0 +1,37 @@
+package com.odeyalo.sonata.common.context;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MalformedContextUriException extends Exception {
+    String uriString;
+
+    public MalformedContextUriException(String uriString) {
+        this.uriString = uriString;
+    }
+
+    public MalformedContextUriException(String message, String uriString) {
+        super(message);
+        this.uriString = uriString;
+    }
+
+    public MalformedContextUriException(String message, Throwable cause, String uriString) {
+        super(message, cause);
+        this.uriString = uriString;
+    }
+
+    public MalformedContextUriException(Throwable cause, String uriString) {
+        super(cause);
+        this.uriString = uriString;
+    }
+
+    public MalformedContextUriException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String uriString) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.uriString = uriString;
+    }
+}
