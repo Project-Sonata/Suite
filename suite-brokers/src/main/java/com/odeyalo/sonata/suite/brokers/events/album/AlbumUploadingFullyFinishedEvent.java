148daf58d9a5b8e0ef9766139bce352ad2054bfd
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2023-11-10 18:05:48 +0200

    Added private constructor (#16)
    Added consttuctor to AlbumUploadingFullyFinishedEvent

--- suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/album/AlbumUploadingFullyFinishedEvent.java
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/album/AlbumUploadingFullyFinishedEvent.java
@@ -5,6 +5,10 @@ import com.odeyalo.sonata.suite.brokers.events.album.data.AlbumFullyUploadedInfo
 
 public class AlbumUploadingFullyFinishedEvent extends AbstractEvent<AlbumFullyUploadedInfo> {
 
+    AlbumUploadingFullyFinishedEvent() {
+        super(null);
+    }
+
     public AlbumUploadingFullyFinishedEvent(AlbumFullyUploadedInfo body) {
         super(body);
     }
