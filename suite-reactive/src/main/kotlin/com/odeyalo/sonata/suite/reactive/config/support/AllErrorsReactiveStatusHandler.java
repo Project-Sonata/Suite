6fde8febf42125396762acb2d539f4cf0bcc9613
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2024-04-21 22:01:28 +0300

    Made a small refactoring. Removed unnecessary methods and logs (#26)
    Made a small refactoring. Removed unnecessary methods and logs

--- suite-reactive/src/main/kotlin/com/odeyalo/sonata/suite/reactive/config/support/AllErrorsReactiveStatusHandler.java
+++ suite-reactive/src/main/kotlin/com/odeyalo/sonata/suite/reactive/config/support/AllErrorsReactiveStatusHandler.java
@@ -1,45 +1,27 @@
 package com.odeyalo.sonata.suite.reactive.config.support;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odeyalo.sonata.suite.reactive.exception.decoder.registry.ClientMethodExceptionHandlerDecoderRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactivefeign.client.ReactiveHttpResponse;
 import reactivefeign.client.statushandler.ReactiveStatusHandler;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
-
 /**
- * ReactiveStatusHandler that handles only HTTP 400 error.
+ * {@link ReactiveStatusHandler} that handles only HTTP errors (from 400 to 600 code).
  */
 public class AllErrorsReactiveStatusHandler implements ReactiveStatusHandler {
     private final ClientMethodExceptionHandlerDecoderRegistry container;
-    protected final ObjectMapper objectMapper;
-    protected final Logger logger = LoggerFactory.getLogger(AllErrorsReactiveStatusHandler.class);
 
-    public AllErrorsReactiveStatusHandler(ObjectMapper objectMapper, ClientMethodExceptionHandlerDecoderRegistry container) {
-        System.out.println("created error decoder");
-        this.objectMapper = objectMapper;
+    public AllErrorsReactiveStatusHandler(ClientMethodExceptionHandlerDecoderRegistry container) {
         this.container = container;
     }
 
     @Override
     public boolean shouldHandle(int status) {
-        return status >= 400;
+        return status >= 400 && status < 600;
     }
 
     @Override
     public Mono<? extends Throwable> decode(String methodName, ReactiveHttpResponse<?> reactiveHttpResponse) {
         return container.getDecoder(methodName, true).handleException(methodName, reactiveHttpResponse);
-    }
-
-    protected <T> Mono<T> parse(byte[] bytes, Class<T> target) {
-        try {
-            return Mono.just(objectMapper.readValue(bytes, target));
-        } catch (IOException e) {
-            logger.error(String.format("An error was occurred during JSON parsing. Target class: %s\n", target.getName()), e);
-            return Mono.error(e);
-        }
     }
 }
