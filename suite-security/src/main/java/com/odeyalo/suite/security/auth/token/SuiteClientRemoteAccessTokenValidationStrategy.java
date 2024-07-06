5bf7af7a87afaac8fdc322f9402d6c2eaf13083b
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2024-07-06 16:18:07 +0300

    Revert "[Suite-Reactive] Fixed bug when composite interceptor returns empty m…"
    This reverts commit 5c454333ea57fd571d76a199564ec41ebbe04f95.

--- suite-security/src/main/java/com/odeyalo/suite/security/auth/token/SuiteClientRemoteAccessTokenValidationStrategy.java
+++ suite-security/src/main/java/com/odeyalo/suite/security/auth/token/SuiteClientRemoteAccessTokenValidationStrategy.java
@@ -4,8 +4,7 @@ import com.odeyalo.sonata.common.authorization.TokenIntrospectionRequest;
 import com.odeyalo.sonata.common.authorization.TokenIntrospectionResponse;
 import com.odeyalo.sonata.suite.reactive.client.ReactiveTokenIntrospectionClient;
 import com.odeyalo.suite.security.auth.token.converter.ValidatedAccessTokenConverter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 /**
@@ -14,7 +13,6 @@ import reactor.core.publisher.Mono;
 public class SuiteClientRemoteAccessTokenValidationStrategy implements RemoteAccessTokenValidationStrategy {
     private final ReactiveTokenIntrospectionClient tokenIntrospectionClient;
     private final ValidatedAccessTokenConverter<TokenIntrospectionResponse> accessTokenConverter;
-    private final Logger logger = LoggerFactory.getLogger(SuiteClientRemoteAccessTokenValidationStrategy.class);
 
     public SuiteClientRemoteAccessTokenValidationStrategy(ReactiveTokenIntrospectionClient tokenIntrospectionClient, ValidatedAccessTokenConverter<TokenIntrospectionResponse> accessTokenConverter) {
         this.tokenIntrospectionClient = tokenIntrospectionClient;
@@ -24,21 +22,7 @@ public class SuiteClientRemoteAccessTokenValidationStrategy implements RemoteAcc
     @Override
     public Mono<ValidatedAccessToken> validateAccessToken(String tokenValue) {
         return tokenIntrospectionClient.introspectToken(TokenIntrospectionRequest.of(tokenValue))
-                .log("suite-strategy")
-                .doOnNext(resp ->
-                        logger.info("""
-                                [OK] Response from remote token validation service:
-                                API status [UP],
-                                Protocol: [HTTP],
-                                API response status code: [{}]
-                                Token has been validated. Token validation result: [{}]""", resp.getStatusCode(), resp.getBody()))
-                .flatMap(r -> {
-                    Mono<TokenIntrospectionResponse> body = r.getBody();
-                    if ( body == null ) {
-                        return Mono.error(new IllegalStateException("Api returned a NULL body as the result"));
-                    }
-                    return body;
-                })
+                .flatMap(r -> r.getBody() != null ? r.getBody() : Mono.empty())
                 .map(accessTokenConverter::convertTo);
     }
 }
