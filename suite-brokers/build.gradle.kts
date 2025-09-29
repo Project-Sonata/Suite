b0d851e83bad253cc8dcad42d838b812ab682c24
odeyalo <theprogrammerinfuture@gmail.com>
2025-09-29 21:36:35 +0300

    Migrate to gradle 'suite-brokers' module

---
+++ suite-brokers/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://maven.pkg.github.com/Project-Sonata/Common")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api(libs.org.projectlombok.lombok)
+    api(libs.com.odeyalo.sonata.suite)
+}
+
+group = "com.odeyalo.sonata.suite"
+version = "0.0.9"
+description = "suite-brokers"
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
