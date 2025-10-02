f318339a4e9ff58c9be5e832e882f7eac5b31a9a
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-02 22:00:22 +0300

    Add track played event

---
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/activity/player/TrackPlayedEvent.java
@@ -0,0 +1,31 @@
+package com.odeyalo.sonata.suite.brokers.events.activity.player;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.odeyalo.sonata.suite.brokers.events.AbstractEvent;
+import com.odeyalo.sonata.suite.brokers.events.activity.player.payload.TrackPlayedPayload;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+
+@EqualsAndHashCode(callSuper = true)
+@Value
+public class TrackPlayedEvent extends AbstractEvent<TrackPlayedPayload> {
+    public static final String EVENT_TYPE = "track_played";
+
+    public TrackPlayedEvent(final TrackPlayedPayload body) {
+        super(body);
+    }
+
+    @JsonCreator
+    public TrackPlayedEvent(final String id,
+                            final long creationTime,
+                            final TrackPlayedPayload body) {
+        super(id, creationTime, body);
+    }
+
+    @Override
+    @NotNull
+    public String getEventType() {
+        return EVENT_TYPE;
+    }
+}
