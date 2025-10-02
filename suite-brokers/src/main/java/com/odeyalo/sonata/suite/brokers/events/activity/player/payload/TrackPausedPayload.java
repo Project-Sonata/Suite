2f703ac8a1cedfe967b2c2c9a8bfcb89c2833d77
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-02 22:01:07 +0300

    Add track paused event

---
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/activity/player/payload/TrackPausedPayload.java
@@ -0,0 +1,28 @@
+package com.odeyalo.sonata.suite.brokers.events.activity.player.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.util.Assert;
+
+@Value
+@Builder
+public class TrackPausedPayload {
+    @NotNull
+    @JsonProperty("user_id")
+    String userId;
+    @NotNull
+    @JsonProperty("track_id")
+    String trackId;
+    int position;
+
+    public TrackPausedPayload(@NotNull final String userId,
+                              @NotNull final String trackId,
+                              final int position) {
+        Assert.isTrue(position > 0, "Position should be positive");
+        this.userId = userId;
+        this.trackId = trackId;
+        this.position = position;
+    }
+}
