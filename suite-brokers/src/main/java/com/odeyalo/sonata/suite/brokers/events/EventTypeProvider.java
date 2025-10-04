79ec16cf95cc34d134e40d89953082e4bfe1ebe2
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2025-10-04 14:14:31 +0300

    PDS-4: AbstractEvent properties are not serialized (#45)
    * Fix invalid condition
    
    * Add getter for AbstractEvent so data is serialized properly

--- suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/EventTypeProvider.java
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/EventTypeProvider.java
@@ -1,5 +1,6 @@
 package com.odeyalo.sonata.suite.brokers.events;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
 
 public interface EventTypeProvider {
@@ -7,6 +8,7 @@ public interface EventTypeProvider {
      * @return unique type of this event that used as identifier
      */
     @NotNull
+    @JsonProperty("event_type")
     String getEventType();
 
 }
--- suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/activity/player/TrackResumedEvent.java
+++ suite-brokers/src/main/java/com/odeyalo/sonata/suite/brokers/events/activity/player/TrackResumedEvent.java
@@ -2,24 +2,24 @@ package com.odeyalo.sonata.suite.brokers.events.activity.player;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.odeyalo.sonata.suite.brokers.events.AbstractEvent;
-import com.odeyalo.sonata.suite.brokers.events.activity.player.payload.TrackSeekPayload;
+import com.odeyalo.sonata.suite.brokers.events.activity.player.payload.TrackPlayedPayload;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
 @EqualsAndHashCode(callSuper = true)
 @Value
-public class TrackResumedEvent extends AbstractEvent<TrackPlayedEvent> {
+public class TrackResumedEvent extends AbstractEvent<TrackPlayedPayload> {
     public static final String EVENT_TYPE = "track_resumed";
 
-    public TrackResumedEvent(final TrackPlayedEvent body) {
+    public TrackResumedEvent(final TrackPlayedPayload body) {
         super(body);
     }
 
     @JsonCreator
     public TrackResumedEvent(final String id,
                              final long creationTime,
-                             final TrackPlayedEvent body) {
+                             final TrackPlayedPayload body) {
         super(id, creationTime, body);
     }
 
